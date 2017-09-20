ea4626573e35685e79ec3b3226563b5c7146d418
therealproprogrammer <therealproprogrammer@gmail.com>
2017-09-19 18:30:46 -0700

    Follows PEP8 more

--- textencodefunc.py
+++ textencodefunc.py
@@ -1,6 +1,5 @@
-
-def LookUpWordValue(words):
-    '''
+def look_up_word_value(words):
+    """
     ---------------------------------------------------------------------
     DESCRIPTION
     Translates the word (string) array into a floating-point value array.
@@ -9,27 +8,27 @@ def LookUpWordValue(words):
     words (string array): The array of words to convert into a floating-
     point value array.
     ---------------------------------------------------------------------
-    '''
-    TheDictionary={}
-    wordNum=0
-    TheListOfWords = open("C:/YourShortListOfWords.txt", "r")
-    TheTextWithin = TheListOfWords.read()
-    for line in TheTextWithin.split('\n'):
-        #print(line+":"+str(wordNum))
-        TheDictionary[line]=wordNum
-        wordNum=wordNum+1
-    LookedUpArray = []
+    """
+    the_dictionary = {}
+    word_num = 0
+    the_list_of_words = open("C:/YourShortListOfWords.txt", "r")
+    the_text_within = the_list_of_words.read()
+    for line in the_text_within.split('\n'):
+        # print(line+":"+str(word_num))
+        the_dictionary[line] = word_num
+        word_num = word_num + 1
+    looked_up_array = []
     for word in words:
-        LookedUpArray.append(int(TheDictionary[word]))
-    #print(LookedUpArray)
-    RealLookedUpArray = []
-    for wordval in LookedUpArray:
-        RealLookedUpArray.append(wordval/10000)
-    return(RealLookedUpArray)
+        looked_up_array.append(int(the_dictionary[word]))
+    # print(looked_up_array)
+    real_looked_up_array = []
+    for word_val in looked_up_array:
+        real_looked_up_array.append(word_val / 10000)
+    return real_looked_up_array
 
 
-def LookUpWordForValue(wordvalues):
-    '''
+def look_up_word_for_value(word_values):
+    """
     ---------------------------------------------------------------------
     DESCRIPTION
     Translates the floating-point value array into a word (string) array.
@@ -38,113 +37,114 @@ def LookUpWordForValue(wordvalues):
     wordvalues (floating-point value array): The array of floating-point
     values to convert into a word (string) array.
     ---------------------------------------------------------------------
-    '''
-    WordListHere = []
-    TheListOfWordsHere = open("C:/YourShortListOfWords.txt", "r")
-    TheWordListWithin = TheListOfWordsHere.read()
-    for line in TheWordListWithin.split('\n'):
-        WordListHere.append(line)
-    OutputWordListHere = []
-    for wordvalue in wordvalues:
-        OutputWordListHere.append(WordListHere[int(wordvalue*10000)])
-    return OutputWordListHere
+    """
+    word_list_here = []
+    the_list_of_words_here = open("C:/YourShortListOfWords.txt", "r")
+    the_word_list_within = the_list_of_words_here.read()
+    for line in the_word_list_within.split('\n'):
+        word_list_here.append(line)
+    output_word_list_here = []
+    for word_value in word_values:
+        output_word_list_here.append(word_list_here[int(word_value * 10000)])
+    return output_word_list_here
 
 
-def IsValidWordArray(WordsToCheck):
-    '''
+def is_valid_word_array(words_to_check):
+    """
     ---------------------------------------------------------------------
     DESCRIPTION
     Checks if the words in the word (string) array are part of the
     dictionary.
     ---------------------------------------------------------------------
     PARAMETERS
-    WordsToCheck (string array): The array of words to check for in the
+    words_to_check (string array): The array of words to check for in the
     dictionary.
     ---------------------------------------------------------------------
-    '''
-    VALID = True
+    """
+    valid = True
     try:
-        LookUpWordValue(WordsToCheck)
+        look_up_word_value(words_to_check)
     except:
-        VALID = False
-    return VALID
+        valid = False
+    return valid
 
 
-def AddWordToDictionary(WordToAdd):
-    '''
+def add_word_to_dictionary(word_to_add):
+    """
     ---------------------------------------------------------------------
     DESCRIPTION
     Adds a word to the dictionary file, if it does not already exist.
     ---------------------------------------------------------------------
     PARAMETERS
-    WordToAdd (string): The word to add to the dictionary.
+    word_to_add (string): The word to add to the dictionary.
     ---------------------------------------------------------------------
-    '''
-    ListOfExistWords = open("C:/YourShortListOfWords.txt", "r")
-    ExistingWords = ListOfExistWords.read()
-    NOTTAKEN = True
-    for ExistLine in ExistingWords.split('\n'):
-        if ExistLine.lower()==WordToAdd:
-            NOTTAKEN = False
-    if NOTTAKEN:
-        ReadyToAdd = open("C:/YourShortListOfWords.txt", "a")
-        ReadyToAdd.write("\n"+WordToAdd.lower())
+    """
+    list_of_exist_words = open("C:/YourShortListOfWords.txt", "r")
+    existing_words = list_of_exist_words.read()
+    not_taken = True
+    for ExistLine in existing_words.split('\n'):
+        if ExistLine.lower() == word_to_add:
+            not_taken = False
+    if not_taken:
+        ready_to_add = open("C:/YourShortListOfWords.txt", "a")
+        ready_to_add.write("\n" + word_to_add.lower())
 
 
-def PadWordArray(WordArrayToPad,InputSize):
-    '''
+def pad_word_array(word_array_to_pad, input_size):
+    """
     ---------------------------------------------------------------------
     DESCRIPTION
     Pads the word array with ^ to reshape it to the network's input size,
     or trims it if necessary. Otherwise, leaves it unchanged.
     ---------------------------------------------------------------------
     PARAMETERS
-    WordArrayToPad (string array): The word array to pad.
-    InputSize (integer): The input size the neural network expects.
+    word_array_to_pad (string array): The word array to pad.
+    input_size (integer): The input size the neural network expects.
     ---------------------------------------------------------------------
-    '''
-    if len(WordArrayToPad)>InputSize:
-        return WordArrayToPad[0:InputSize]
-    elif len(WordArrayToPad)==InputSize:
-        return WordArrayToPad
-    elif len(WordArrayToPad)<InputSize:
-        PaddedWordArray = WordArrayToPad
-        for PadChar in range(InputSize-len(WordArrayToPad)):
-            PaddedWordArray.append("^")
-        return PaddedWordArray
+    """
+    if len(word_array_to_pad) > input_size:
+        return word_array_to_pad[0:input_size]
+    elif len(word_array_to_pad) == input_size:
+        return word_array_to_pad
+    elif len(word_array_to_pad) < input_size:
+        padded_word_array = word_array_to_pad
+        for PadChar in range(input_size - len(word_array_to_pad)):
+            padded_word_array.append("^")
+        return padded_word_array
 
 
-def EasyConvertSentenceToValues(SentenceArray,InputSize):
-    '''
+def easy_convert_sentence_to_values(sentence_array, input_size):
+    """
     ---------------------------------------------------------------------
     DESCRIPTION
     Converts the array of sentences to an array of word value arrays. If
     necessary, they might be padded.
     ---------------------------------------------------------------------
     PARAMETERS
-    SentenceArray (string array): The sentence array to convert.
-    InputSize (integer): The input size the neural network expects.
+    sentence_array (string array): The sentence array to convert.
+    input_size (integer): The input size the neural network expects.
     ---------------------------------------------------------------------
-    '''
-    ArrOfTokenWrdArrs=[]
-    #Tokenizes each sentence in ArrOfTokenWrdArrs
+    """
+    arr_of_token_wrd_arrs = []
+    # Tokenizes each sentence in arr_of_token_wrd_arrs
     import nltk
-    for SentenceToTokenize in SentenceArray:
-        ArrOfTokenWrdArrs.append(PadWordArray(nltk.word_tokenize(SentenceToTokenize),InputSize))
-        
-    #Checks the validity of ArrOfTokenWrdArrs, extending the dictionary if necessary
-    for WordArray in ArrOfTokenWrdArrs:
-        for Word in WordArray:
-            if IsValidWordArray([Word]):
-                print(Word+" is a valid word.")
-            else:
-                AddWordToDictionary(Word)
+    for SentenceToTokenize in sentence_array:
+        arr_of_token_wrd_arrs.append(pad_word_array(nltk.word_tokenize(SentenceToTokenize), input_size))
 
-    #Converts ArrOfTokenWrdArrs to an array of word value arrays
-    ArrOfWrdValArrs=[]
-    for WordArrayToConvert in ArrOfTokenWrdArrs:
-        ArrOfWrdValArrs.append(LookUpWordValue(WordArrayToConvert))
-    return ArrOfWrdValArrs
+    # Checks the validity of arr_of_token_wrd_arrs, extending the dictionary if necessary
+    for WordArray in arr_of_token_wrd_arrs:
+        for Word in WordArray:
+            if is_valid_word_array([Word]):
+                print(Word + " is a valid word.")
+            else:
+                add_word_to_dictionary(Word)
+
+    # Converts arr_of_token_wrd_arrs to an array of word value arrays
+    arr_of_wrd_val_arrs = []
+    for WordArrayToConvert in arr_of_token_wrd_arrs:
+        arr_of_wrd_val_arrs.append(look_up_word_value(WordArrayToConvert))
+    return arr_of_wrd_val_arrs
+
 
 '''
 #Keras Example Below
